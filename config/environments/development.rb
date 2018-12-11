2ddd35d86cb47882793bebb83410feae99457205
Andrew Kane <andrew@chartkick.com>
2018-12-11 00:51:44 -0800

    Added support for Slack

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
 
-  config.action_mailer.default_url_options = {host: "localhost", port: 5000}
+  config.action_mailer.default_url_options = {host: "localhost", port: 3000}
 
   config.action_mailer.default_options = {from: "hi@test.com"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2016_07_10_032559) do
+ActiveRecord::Schema.define(version: 2018_12_11_064618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2016_07_10_032559) do
     t.boolean "invert"
     t.string "check_type"
     t.text "message"
+    t.text "slack_channels"
     t.index ["query_id"], name: "index_blazer_checks_on_query_id"
   end
 
